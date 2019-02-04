2f7ac6b7739895f1cfe2c29b5851784169aa0d04
Bruno Freitas <brunosfreitas@gmail.com>
2019-02-04 00:43:36 -0200

    git ignore

---
+++ config/env/development.env.js
@@ -0,0 +1,10 @@
+module.exports = {
+	env: 'development',
+	db: 'testedb',
+	dialect: 'postgres',
+	username: 'YOURUSER',
+	password: 'YOURPASS',
+	host: 'testedb.crtgcjmmv95e.us-east-2.rds.amazonaws.com',
+	pgPort: 5432,
+	dbURL: 'postgres://bruno:bruno123@testedb.crtgcjmmv95e.us-east-2.rds.amazonaws.com:5432/testedb',
+};
